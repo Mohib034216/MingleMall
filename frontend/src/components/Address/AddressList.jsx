dce677bf206d8e4ff2f49fcb573fcdd4dcf81f2c
Mohib Ali <mohibalikhan16@gmail.com>
2025-01-20 01:56:14 +0500

    FILES UPDATED

--- frontend/src/components/Address/AddressList.jsx
+++ frontend/src/components/Address/AddressList.jsx
@@ -1,20 +1,74 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./Address.css"
 import { useDispatch, useSelector } from 'react-redux';
 import {AddressListAction} from "../../feadured/Auth";
 import  AddressForm from "../Address/AddressForm";
+import Modal from '../Modal/Modal';
+
 
 
 function AddressList() {
   const  dispatch = useDispatch();
-  const inputRef = useRef(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  const [modalTitle, setModalTitle] = useState(null);
 
-  const handleAddressChange  = () => {
-    if(inputRef.current){
-      inputRef.current.focus();
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setModalContent(null);
+  };
 
-    }
-  }
+  const handleShowAddressList = () => {
+    setModalTitle("List Address")
+
+    setModalContent(
+      
+       AddressBook['Shipping'] && AddressBook['Shipping'].label ?
+       <div className="address-list-info">
+       <span className='address-list-tag-label'>{AddressBook['Shipping'] && AddressBook['Shipping'].label}</span>
+       <span className='address-list-line'>{AddressBook['Shipping'] && AddressBook['Shipping'].address}</span>
+     </div>
+        :
+        <></>
+    );
+    setIsModalOpen(true);
+  };
+
+  const handleShowAddressForm = () => {
+    setModalTitle("Add Address")
+    setModalContent(
+      <form>
+        <label htmlFor="fullName">Full Name</label>
+        <input type="text" id="fullName" placeholder="Enter full name" />
+
+        <label htmlFor="phone">Phone Number</label>
+        <input type="text" id="phone" placeholder="Enter phone number" />
+
+        <label htmlFor="address">Address</label>
+        <textarea id="address" placeholder="Enter address"></textarea>
+
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" placeholder="Enter city" />
+
+        <label htmlFor="province">Province</label>
+        <select id="province">
+          <option value="">Select Province</option>
+          <option value="Sindh">Sindh</option>
+          <option value="Punjab">Punjab</option>
+          <option value="Balochistan">Balochistan</option>
+          <option value="KPK">Khyber Pakhtunkhwa</option>
+        </select>
+        <button type="submit" style={{ marginTop: "10px" }}>
+          Save
+        </button>
+        <button className='' onClick={handleCloseModal} style={{ marginTop: "10px" }}>
+          Close
+        </button>
+      </form>
+    );
+    setIsModalOpen(true);
+  };
+  
 
   const {userInfo, AddressBook} = useSelector(state => state.auth);
   // console.log(userInfo)
@@ -30,7 +84,7 @@ function AddressList() {
         <h1>Delivery Information</h1>
       <div className="address-list-title">
           <p className='address-list-title-txt'>Shipping & Billing</p>
-          <a
+          {/* <a
               type="a"
               className="address-list-title-edit"
               data-bs-toggle="modal"
@@ -38,7 +92,11 @@ function AddressList() {
               onClick={handleAddressChange}
             >
               Edit
-            </a>
+            </a> */}
+            {/* Edit Button */}
+      <a className="address-list-title-edit" onClick={handleShowAddressList}>
+        Edit
+      </a>
           {/* <a onClick={handleAddressChange} className='address-list-title-edit'>Edit</a> */}
 
       </div>
@@ -46,26 +104,15 @@ function AddressList() {
         <span className='address-list-tag-label'>{AddressBook['Shipping'] && AddressBook['Shipping'].label}</span>
         <span className='address-list-line'>{AddressBook['Shipping'] && AddressBook['Shipping'].address}</span>
       </div>
-      {/* <!-- Modal --> */}
-<div class="modal fade" id="addressModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        title={modalTitle ? modalTitle : ""}
+      >
+        {modalContent}
+      </Modal>
+
+    
       
     </div>
     
